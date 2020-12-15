45bb129cead1a81eee3ebf8b7c8913f374c319b7
Administrator <Administrator@USER-20161120BQ>
2020-12-15 19:31:06 +0800

    1

--- src/main/java/com/fydk/productone/aspect/MyInterceptor.java
+++ src/main/java/com/fydk/productone/aspect/MyInterceptor.java
@@ -53,7 +53,6 @@ public class MyInterceptor implements HandlerInterceptor {
 				redisTemplate.opsForValue().set(key, jo,10,TimeUnit.MINUTES);
 				return true;
 			}else {
-				
 				return false;
 			}
 		}
